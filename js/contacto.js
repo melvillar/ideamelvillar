fdaac2dd5fa45669f510a59e15e624f0a2064372
melvillar <villar.melanie.araceli@gmail.com>
2022-07-13 20:29:35 -0300

    primer desafío complementario JavaScript

--- js/contacto.js
+++ js/contacto.js
@@ -9,18 +9,31 @@ let precioServicioB = 10000
 let nombreServicioC = "Asesoría"
 let precioServicioC = 20000
 
-let cotizacion1 = prompt ("¿Qué servicio es el que te interesa cotizar?\nRedacción \nMarketing Digital \nAsesoría")
+let tiposServicios = prompt ("¿Cuántos servicios estás interesado en cotizar?\nActualmente tengo calendario abierto para\nRedacción\nMarketing Digital\nAsesoría")
+let cotizacionAprox = 0;
+
+
+for ( let i = 0 ; i < tiposServicios; i++ ){
+
+let cotizacion1 = prompt ("¿Qué servicio es el que te interesa cotizar?\nRedacción\nMarketing Digital\nAsesoría")
 
 if (cotizacion1 == "Redacción"){
+    cotizacionAprox += precioServicioA
     alert ("El servicio tiene un costo aproximado de $" + precioServicioA)
+    
 }
 else if (cotizacion1 == "Marketing Digital"){
+    cotizacionAprox += precioServicioB
     alert ("El servicio tiene un costo aproximado de $" + precioServicioB)
 }
 else if (cotizacion1 == "Asesoría"){
+    cotizacionAprox += precioServicioC
     alert ("El servicio tiene un costo aproximado de $" + precioServicioC)
 }
 else {
     prompt ("Dejame tu consulta específica y un mail, y te voy a estar respondiendo a la brevedad")
+    break; 
 }
+}
+alert ("El presupuesto aproximado por los servicios consultados es de $" + cotizacionAprox)
 prompt ("Gracias por contactarte conmigo, dejame un mail y me voy a estar comunicando a la brevedad")

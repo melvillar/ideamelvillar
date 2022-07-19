802a8fa5e062e9cb700524f08e8cfc8c66120029
melvillar <villar.melanie.araceli@gmail.com>
2022-07-19 00:24:18 -0300

    Update contacto.js

--- js/contacto.js
+++ js/contacto.js
@@ -9,31 +9,35 @@ let precioServicioB = 10000
 let nombreServicioC = "Asesoría"
 let precioServicioC = 20000
 
-let tiposServicios = prompt ("¿Cuántos servicios estás interesado en cotizar?\nActualmente tengo calendario abierto para\nRedacción\nMarketing Digital\nAsesoría")
 let cotizacionAprox = 0;
 
+function descuentoAgosto (precio, descuento){
+    return precio * porcentaje
+    alert ("El servicio tiene un costo aproximado de $" + cotizacionAprox);
+}
 
-for ( let i = 0 ; i < tiposServicios; i++ ){
+let tiposServicios = prompt ("¿Cuántos servicios estás interesado en cotizar?\nActualmente tengo calendario abierto para\nRedacción\nMarketing Digital\nAsesoría")
+let cotizacion1 = prompt ("¿Qué servicio es el que te interesa cotizar?\nRedacción\nMarketing Digital\nAsesoría").toLowerCase()
 
-let cotizacion1 = prompt ("¿Qué servicio es el que te interesa cotizar?\nRedacción\nMarketing Digital\nAsesoría")
+for ( let i = 0 ; i < tiposServicios; i++ ){ 
+    if (cotizacion1 == "Redacción"){
+        cotizacionAprox += precioServicioA
+        descuentoJulio (precioServicioA, 0.15) 
+    }
+    else if (cotizacion1 == "Marketing Digital"){
+        cotizacionAprox += precioServicioB
+        descuentoJulio (precioServicioB, 0.10) 
+    }
+    else if (cotizacion1 == "Asesoría"){
+        cotizacionAprox += precioServicioC
+        descuentoJulio (precioServicioB, 0.20) 
+    }
+    else {
+        prompt ("Dejame tu consulta específica y un mail, y te voy a estar respondiendo a la brevedad")
+        break; 
+    }
+    }
+    alert ("El presupuesto total aproximado por los servicios consultados es de $" + cotizacionAprox)
+    prompt ("Gracias por contactarte conmigo, dejame un mail y me voy a estar comunicando a la brevedad")
 
-if (cotizacion1 == "Redacción"){
-    cotizacionAprox += precioServicioA
-    alert ("El servicio tiene un costo aproximado de $" + precioServicioA)
-    
-}
-else if (cotizacion1 == "Marketing Digital"){
-    cotizacionAprox += precioServicioB
-    alert ("El servicio tiene un costo aproximado de $" + precioServicioB)
-}
-else if (cotizacion1 == "Asesoría"){
-    cotizacionAprox += precioServicioC
-    alert ("El servicio tiene un costo aproximado de $" + precioServicioC)
-}
-else {
-    prompt ("Dejame tu consulta específica y un mail, y te voy a estar respondiendo a la brevedad")
-    break; 
-}
-}
-alert ("El presupuesto aproximado por los servicios consultados es de $" + cotizacionAprox)
-prompt ("Gracias por contactarte conmigo, dejame un mail y me voy a estar comunicando a la brevedad")
+

89e5a6acb29f7e716a3231f92a9b7dc539b77967
melvillar <villar.melanie.araceli@gmail.com>
2022-07-25 12:43:41 -0300

    ya anda el array, ahora hay que arreglar de nuevo el switch

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,58 +1,62 @@
 //Cotización de servicios ofrecidos por Melanie Villar//
 
+let nombreServicioA = "Redaccion";
+let precioServicioA = 15000;
+
+let nombreServicioB = "Marketing";
+let precioServicioB = 10000;
+
+let nombreServicioC = "Asesoria";
+let precioServicioC = 20000;
+
 let cotizacionAprox = 0;
 let descuento = 0; 
 
 function descuentoJulio (cotizacionAprox, descuento){
-    const calculoDescuento = cotizacionAprox - (cotizacionAprox * descuento)}
+    const calculoDescuento = cotizacionAprox - (cotizacionAprox * descuento)
+};
 
-let resultadoDescuento = descuentoJulio (cotizacionAprox, descuento)
+let resultadoDescuento = descuentoJulio (cotizacionAprox, descuento);
 
 class Servicios {
     constructor (nombre, precio){
         this.nombre = nombre;
         this.precio = precio;
     }
-}
-const servicioA = new Servicios (Redaccion, 15000)
-const servicioB = new Servicios (Marketing, 10000)
-const servicioC = new Servicios (Asesoria, 20000)
+};
 
-let serviciosGenerales = [servicioA, servicioB, servicioC]
+const servicioA = new Servicios (nombreServicioA, precioServicioA);
+const servicioB = new Servicios (nombreServicioB, precioServicioB);
+const servicioC = new Servicios (nombreServicioC, precioServicioC);
 
-let nombresServicios = []
+let serviciosGenerales = [servicioA, servicioB, servicioC];
 
-function listaNombresServicios (){
-    for (const servicio of serviciosGenerales){
-        nombresServicios.push (servicio.nombre)
-    }
-}
+const listaNombres = serviciosGenerales.map(servicio => servicio.nombre);
+const nombresServicios = (listaNombres);
 
-listaNombresServicios()
-
-let cotizacion1 = prompt ("¿Cuáles son servicios que estás interesado en cotizar?\nActualmente tengo calendario abierto para:" + nombresServicios.join("\n "))
+let cotizacion1 = prompt ("¿Cuáles son servicios que estás interesado en cotizar?\nActualmente tengo calendario abierto para:" + nombresServicios);
 
 switch (cotizacion1){
     case "Redaccion":
         cotizacionAprox += (servicioA.precio) ;
         descuentoJulio (cotizacionAprox, 0.15)
-        alert ("El servicio tiene un costo aproximado de $" + resultadoDescuento);
+        alert ("Por descuento, el servicio te queda a solo $" + calculoDescuento + "(\nVálido por todo el mes de julio)");
         break;
     case "Marketing":
         cotizacionAprox += (servicioB.precio);
         descuentoJulio (cotizacionAprox, 0.1)
-        alert ("El servicio tiene un costo aproximado de $" + resultadoDescuento)
+        alert ("Por descuento, el servicio te queda a solo $" + calculoDescuento + "(\nVálido por todo el mes de julio)");
         break;
     case "Asesoria":
         cotizacionAprox += (servicioC.precio);
         descuentoJulio (cotizacionAprox, 0.2)
-        alert ("El servicio tiene un costo aproximado de $" + resultadoDescuento)
+        alert ("Por descuento, el servicio te queda a solo $" + calculoDescuento + "(\nVálido por todo el mes de julio)");
         break;
     default:
         prompt ("Dejame tu consulta específica y un mail, te voy a estar respondiendo a la brevedad");
         break;
-}
+};
 
-alert ("El presupuesto total aproximado por los servicios consultados es de $" + cotizacionAprox)
+alert ("El presupuesto total aproximado por los servicios consultados es de $" + cotizacionAprox);
 
-prompt ("Gracias por contactarte conmigo, dejame un mail y me voy a estar comunicando a la brevedad")
+prompt ("Gracias por contactarte conmigo, dejame un mail y me voy a estar comunicando a la brevedad");
